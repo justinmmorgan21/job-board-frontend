eace000392adcbcdeea733c89e6f65df67a38acf
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-01 20:12:10 -0500

    added companies show page and dummy company data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { JobsIndexPage } from './JobsIndexPage';
+import { Companies}
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 
@@ -15,7 +16,67 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
           </div>
         ),
         children: [
-        { 
+          {
+            path: '/companies',
+            element: <CompaniesIndex />,
+            loader: () => {
+              return [
+                {
+                  id: 5,
+                  name: "Applebees",
+                  logo: "aplbesLogo.jpg",
+                  description: "funnerest place to work ever",
+                  jobs: [
+                    {
+                      id: 1,
+                      company_id: 5,
+                      title: "sales manager",
+                      description: "oversee a team of salespeople",
+                      url: "sales_jobs@money.com",
+                      location: "Los Angeles, CA",
+                      active: true,
+                      salary_range: "$80k - $130k"
+                    },
+                    {
+                      id: 3,
+                      company_id: 5,
+                      title: "transaction compliance officer",
+                      description: "keep people honest",
+                      url: "hr_jobs@money.com",
+                      location: "Dallas, TX",
+                      active: true,
+                      salary_range: "$50k - $55k"
+                    }
+                  ]
+                },
+                {
+                  id: 7,
+                  name: "Foot Locker",
+                  logo: "fl.png",
+                  description: "selling shoes is fun, yay!",
+                  jobs: [
+                    {
+                      id: 2,
+                      company_id: 7,
+                      title: "IT dept",
+                      description: "help people find the power switch",
+                      url: "jobs@jobs.gov",
+                      location: "New York City, NY",
+                      active: true,
+                      salary_range: "$90k - $110k"
+                    }
+                  ]
+                }
+              ]
+            }
+            // axios.get(`http://localhost:3000/companies.json`).then(response => response.data)
+          },
+          {
+            path: '/companies/:id',
+            element: <CompaniesShowPage />,
+            loader: ({ params }) => axios.get(`http://localhost:3000/companies/${params.id}.json`).then(response => response.data)
+          },
+          { 
           path: '/',
           element: <JobsIndexPage />,
           loader: () => {
