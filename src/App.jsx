7c98403670db50acac32ff248d4ba31cbd8731c9
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-02 07:49:57 -0500

    added [0] to the loaderData to access the one company element coming into the company show

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,18 @@ import axios from 'axios';
                   name: "Foot Locker",
                   logo: "fl.png",
                   description: "selling shoes is fun, yay!",
-                  jobs: []
+                  jobs: [
+                    {
+                      id: 2,
+                      company_id: 7,
+                      title: "IT dept",
+                      description: "help people find the power switch",
+                      url: "jobs@jobs.gov",
+                      location: "New York City, NY",
+                      active: true,
+                      salary_range: "$90k - $110k"
+                    }
+                  ]
                 }
               ]
             }
@@ -63,7 +74,39 @@ import axios from 'axios';
           {
             path: '/companies/:id',
             element: <CompaniesShowPage />,
-            loader: ({ params }) => axios.get(`http://localhost:3000/companies/${params.id}.json`).then(response => response.data)
+            loader: () => {
+              return [
+                {
+                  id: 5,
+                  name: "Applebees",
+                  logo: "aplbesLogo.jpg",
+                  description: "funnerest place to work ever",
+                  jobs: [
+                    {
+                      id: 1,
+                      company_id: 5,
+                      title: "sales manager",
+                      description: "oversee a team of salespeople",
+                      url: "sales_jobs@money.com",
+                      location: "Los Angeles, CA",
+                      active: true,
+                      salary_range: "$80k - $130k"
+                    },
+                    {
+                      id: 3,
+                      company_id: 5,
+                      title: "transaction compliance officer",
+                      description: "keep people honest",
+                      url: "hr_jobs@money.com",
+                      location: "Dallas, TX",
+                      active: true,
+                      salary_range: "$50k - $55k"
+                    }
+                  ]
+                }
+              ]
+            }
+              // axios.get(`http://localhost:3000/companies/${params.id}.json`).then(response => response.data)
           },
           { 
           path: '/',
